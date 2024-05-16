b1ee6b6316b430c6ac68536ad93259d3173f078a
Melvin Viougea <mviougea@icloud.com>
2024-05-16 22:18:24 +0200

    update auth login ok

--- services/AxiosService.tsx
+++ services/AxiosService.tsx
@@ -1,11 +1,14 @@
 import axios from "redaxios";
 import {cookies} from "next/headers";
 
+const cookieStore = cookies()
+const auth = cookieStore.get('auth')
+
 const Axios = axios.create({
     baseURL: process.env.API_URL,
     headers: {
         "Content-type": "application/json",
-        "Cookie": cookies( ).toString(),
+        "Authorization": `Bearer ${auth}`,
     }
 });
 
