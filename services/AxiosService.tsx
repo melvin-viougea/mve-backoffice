79e408dd3fc5d1bba1bc0178f2b73e80ab5d2e29
Melvin Viougea <mviougea@icloud.com>
2024-05-16 22:34:12 +0200

    update for cookie

--- services/AxiosService.tsx
+++ services/AxiosService.tsx
@@ -1,16 +1,10 @@
-'use server';
-
 import axios from "redaxios";
-import {cookies} from "next/headers";
-
-const cookieStore = cookies()
-const auth = cookieStore.get('auth')
 
 const Axios = axios.create({
     baseURL: process.env.API_URL,
     headers: {
         "Content-type": "application/json",
-        "Authorization": `Bearer ${auth}`,
+        //"Authorization": `Bearer ${auth}`,
     }
 });
 
